9e2061564ed4bcd9b6b536c84f88f0c538c73ba4
Michal Kowalczyk <contact@alphadev.pl>
2024-10-03 13:04:01 +0200

    Unit test fix

--- database/migrations/2024_10_02_153031_create_league_table.php
+++ database/migrations/2024_10_02_153031_create_league_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('league', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('team_id')->constrained('teams');
+            $table->foreignId('team_id')->default(1)->constrained('teams');
             $table->integer('played')->default(0);
             $table->integer('won')->default(0);
             $table->integer('drawn')->default(0);
