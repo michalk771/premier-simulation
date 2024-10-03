820c7deb9c435fbb102fa923018e4ea67f7beb95
Michal Kowalczyk <contact@alphadev.pl>
2024-10-03 08:58:48 +0200

    init commit

---
+++ app/Models/League.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Team;
+
+class League extends Model
+{
+    use HasFactory;
+
+    protected $table = 'league';
+
+    protected $fillable = ['team_id', 'played', 'won', 'drawn', 'lost', 'goals_for', 'goals_against', 'goal_difference', 'points'];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+}
